97aa60c6f8df4d92d0084cff23c1ea7cf536c8d3
testicles <suckmybals@gmail.com>
2016-01-19 23:25:22 +0000

    Added ec2 ami defaults to install scripts -- should work reallllllll good.

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
+
+# On EC2 AMI Defaults
+#export TORCH_INCLUDE=$(echo ~/torch/include)
+#export TORCH_LIBS=$(echo ~/torch/lib)
+#export TORCH_BIN=$(echo ~/torch/bin)
+#export LD_LIBRARY_PATH=$TORCH_LIBS:$LD_LIBRARY_PATH
+
 export TORCH_INCLUDE=$(echo ~/torch/install/include)
 export TORCH_LIBS=$(echo ~/torch/install/lib)
 export TORCH_BIN=$(echo ~/torch/build/exe/luajit-rocks/luajit-2.1/)
+
 
 echo "Include locaiton: $TORCH_INCLUDE"
 luarocks make;
